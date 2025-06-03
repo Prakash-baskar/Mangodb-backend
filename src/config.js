8a3923d72eed41e9de1c80f6acd40920f413bb1b
Prakash <Prakashprakas2000@gmail.com>
2025-06-03 16:40:51 +0530

    mongodb connect with  login complete

--- src/config.js
+++ src/config.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect()
+const connect = mongoose.connect('mongodb+srv://prakashbaskar67:Pks672000@auth.mm5wwsr.mongodb.net/?retryWrites=true&w=majority&appName=Auth');
+
+connect.then(() => {
+    console.log("Connected to MongoDB successfully");
+})
+.catch( () =>{
+    console.error("Error connecting to MongoDB");
+})
+
+const LoginSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+     
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const collection = new mongoose.model("product", LoginSchema);
+
+module.exports = collection;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const pasth = require('path');
 const bcrypt = require('bcrypt');
-
+const collection = require('./config')
 const app = express();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); 
 
 app.get('/', (req, res) => {
     res.render('login');    
@@ -17,6 +18,31 @@ app.get('/signup', (req, res) => {
 });
 
 const port = 5000;
+
+
+
+
+
+app.post('/login', async (req, res) => {
+    try{
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        const newUser = new collection({
+            username: username,
+            password: hashedPassword
+        });
+        
+        await newUser.save();
+        res.redirect('/');
+
+    }catch(err) {
+        console.error("Error during signup:", err);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 
